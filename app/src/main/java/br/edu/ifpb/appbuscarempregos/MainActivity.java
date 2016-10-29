db5c9f7a0f4f0c8c4ec21cee05e033273240fd3d
Henrique Martins <enrque.ns@gmail.com>
2016-10-29 11:30:42 -0300

    Menus adicionados

--- app/src/main/java/br/edu/ifpb/appbuscarempregos/MainActivity.java
+++ app/src/main/java/br/edu/ifpb/appbuscarempregos/MainActivity.java
@@ -2,12 +2,18 @@ package br.edu.ifpb.appbuscarempregos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button bBrasil;
+    private Button bCG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bBrasil.setOnClickListener(null);
+        bCG.setOnClickListener(null);
     }
 }
---
+++ app/src/main/java/br/edu/ifpb/appbuscarempregos/listeners/ListarBrasilOnClickListener.java
@@ -0,0 +1,14 @@
+package br.edu.ifpb.appbuscarempregos.listeners;
+
+import android.view.View;
+
+/**
+ * Created by Henrique on 29/10/2016.
+ */
+
+public class ListarBrasilOnClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        
+    }
+}
