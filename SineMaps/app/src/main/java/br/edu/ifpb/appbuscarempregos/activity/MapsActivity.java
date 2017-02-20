ee48b263f9f03a1eb8dee085a765a6322c51e700
Henrique Martins <henrikemaartins21@hotmail.com>
2017-02-20 19:56:40 -0300

    Mudança nos icones

--- SineMaps/app/src/main/java/br/edu/ifpb/appbuscarempregos/activity/MapsActivity.java
+++ SineMaps/app/src/main/java/br/edu/ifpb/appbuscarempregos/activity/MapsActivity.java
@@ -39,7 +39,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         List<Sine> sines = (ArrayList<Sine>) getIntent().getSerializableExtra("lista");
 
         LatLng CG = new LatLng(getIntent().getDoubleExtra("lat", -7.219204), getIntent().getDoubleExtra("long", -35.882901));
-        mMap.addMarker(new MarkerOptions().position(CG).title("Ponto de referência"));
+        mMap.addMarker(new MarkerOptions()
+                .position(CG)
+                .title("Ponto de referência")
+                .snippet("Onde Você está")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_sine_red)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(CG));
 
         for (int i = 0; i < sines.size(); i++) {
@@ -48,7 +52,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .position(ponto)
                     .title("Nome: " + sines.get(i).getNome())
                     .snippet("Telefone: " + sines.get(i).getTelefone())
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_sine));
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_sine_blue));
+
             mMap.addMarker(options);
             mMap.setMinZoomPreference(8);
         }
